52db8d822b4b491845f2160cb5cc6fe0f97d6ea1
Naphat Nuansri <naphat.nu@mail.kmutt.ac.th>
2021-03-04 17:37:04 +0700

    --

--- practice/src/main/java/sit/int204/practice/controllers/ProductController.java
+++ practice/src/main/java/sit/int204/practice/controllers/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import sit.int204.practice.Product;
 import sit.int204.repositories.ProductRepository;
 
 @Controller
@@ -16,5 +17,22 @@ public class ProductController {
 	public String product(Model model) {
 		model.addAttribute("products",productRepository.findAll());
 		return "product";
+	}
+	
+	@RequestMapping("/show/{id}")
+	public String show(Model model){
+		return "show";		
+	}
+	
+	@RequestMapping("/create")
+	public String create(Model model) {
+		return "create";
+	}
+	
+	@RequestMapping("/save")
+	public String save(Product product,Model model) {
+		productRepository.save(product);
+		model.addAttribute("product",product);
+		return "redirect:/show" + product.getId();
 	}
 }
